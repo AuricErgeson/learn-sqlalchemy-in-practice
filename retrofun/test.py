798bbcdf972a5043fc18aca6bf5b277c86f278a2
AuricErgesonNitonde <a.e.nitonde@4pl-intermodal.com>
2026-08-24 17:03:37 +0200

    deleted files

--- retrofun/test.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open("products.csv") as f:
-    reader = csv.DictReader(f)
-    for row in reader:
-        manufacturer_name = row.pop('manufacturer')
-        print(manufacturer_name)
-
-
