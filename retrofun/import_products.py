944bcc69e23349e140b5494b51dffbc669a89d69
AuricErgesonNitonde <a.e.nitonde@4pl-intermodal.com>
2026-09-04 14:47:55 +0200

    tested the queries, added indexes, did exercises

--- retrofun/import_products.py
+++ retrofun/import_products.py
@@ -1,19 +1,31 @@
 import csv
 from db import Model , Session, engine
-from models import Product
+from models import Product, Manufacturer
 
 def main():
-    Model.metadata.drop_all(engine)
+    Model.metadata.drop_all(engine) # warning: this deletes all data!
     Model.metadata.create_all(engine)
 
     with Session() as session:
         with session.begin():
-            with open("products.csv") as f:
+            with open('products.csv') as f:
                 reader = csv.DictReader(f)
+                all_manufacturers = {}
                 for row in reader:
-                    product = Product(**row)
-                    session.add(product)
+                    row['year'] = int(row['year'])
+
+                    manufacturer = row.pop('manufacturer')
+                    p = Product(**row)
+
+                    if manufacturer not in all_manufacturers:
+                        m = Manufacturer(name=manufacturer)
+                        session.add(m)
+                        all_manufacturers[manufacturer] = m
+
+                    all_manufacturers[manufacturer].products.append(p)
 
 
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     main()
--- retrofun/models.py
+++ retrofun/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String,ForeignKey
+from typing import List
+from sqlalchemy.orm import Mapped, mapped_column,relationship
 from db import Model
 
 class Product(Model):
@@ -7,10 +8,24 @@ class Product(Model):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(64),index=True,unique=True)
-    manufacturer: Mapped[str] = mapped_column(String(64),index=True)
+    manufacturer_id : Mapped[int] = mapped_column(ForeignKey('manufacturers.id'),index=True)
     year: Mapped[int] = mapped_column(index=True)
     country: Mapped[str] = mapped_column(String(32))
     cpu: Mapped[str] = mapped_column(String(32))
 
+    manufacturer: Mapped['Manufacturer'] = relationship(back_populates='products')
+
     def __repr__(self):
         return f'Product({self.id}, "{self.name}")'
+
+
+class Manufacturer(Model):
+    __tablename__ = 'manufacturers'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(64),index=True,unique=True)
+
+    products: Mapped[List['Product']] = relationship(back_populates='manufacturer',cascade='all,delete-orphan')
+
+
+    def __repr__(self):
+        return f'Manufacturer({self.id}, "{self.name}")'
